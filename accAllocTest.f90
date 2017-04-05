6f13565764e73b4ebeda095fd8c269d20be38135
cosunae <carlos.osuna@env.ethz.ch>
2017-04-05 14:06:11 +0200

    fixes to the cuda malloc call

--- accAllocTest.f90
+++ accAllocTest.f90
@@ -2,13 +2,13 @@ module my_cuda_routines
   implicit none
   public
   interface
-     function cuda_malloc(ptr, n) result(istat) &
-          bind(C, name="cuda_malloc")
+     function my_cuda_malloc(ptr, n) result(istat) &
+          bind(C, name="my_cuda_malloc")
      use iso_c_binding
      TYPE(C_PTR)                          :: ptr
      integer(C_INT), intent(in), value    :: n
      integer(C_INT)                       :: istat
-     end function cuda_malloc
+     end function my_cuda_malloc
   end interface
 end module my_cuda_routines
 
@@ -58,7 +58,7 @@ program acc_alloc_test
   real :: vres
   
   
-  res = cuda_malloc(myptr, n*n) 
+  res = my_cuda_malloc(myptr, n*n) 
   if(res .ne. 0) then
      print *, "Error: Allocation"
      stop
--- cuda_malloc.cpp
+++ cuda_malloc.cpp
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 extern "C" {
-  int  cuda_malloc(size_t** a, int n){
-     int err = cuda_malloc(a, n*sizeof(double));
+  int  my_cuda_malloc(size_t** a, int n){
+     int err = cudaMalloc(a, n*sizeof(double));
      return(err);
   }
 }
